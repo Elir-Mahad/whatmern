3d4df58c1613ed92fc73a52312eade2883a57b0d
code <mmahad280@gmail.com>
2020-09-08 20:14:11 -0400

    added icons to sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./Sidebar.css";
 
+// material ui
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+
 function Sidebar() {
 	return (
 		<div className="sidebar">
 			<h1>Side bar</h1>
-			<div className="sidebar_header"></div>
+			<div className="sidebar_header">
+				<div className="sidebar_headerRight">
+					<IconButton>
+						{/* When you wrap an icon inside IconButton, 
+						when the icon is hovered over and clicked 
+						it will have a little animation */}
+						<DonutLargeIcon />
+					</IconButton>
+				</div>
+			</div>
 		</div>
 	);
 }
