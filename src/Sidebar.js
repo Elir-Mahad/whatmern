ac3abae34f9fcd6b86dbc5b9f17bb74f3198fc06
code <mmahad280@gmail.com>
2020-09-08 20:49:12 -0400

    added the sidebar chat component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,6 +7,7 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
 import { Avatar, IconButton } from "@material-ui/core";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
 	return (
@@ -14,6 +15,7 @@ function Sidebar() {
 			<h1>Side bar</h1>
 			<div className="sidebar_header">
 				<Avatar src="https://i.pinimg.com/280x280_RS/c5/db/7f/c5db7f245e3cd39c4d6c518dafa97e59.jpg" />
+				{/*  */}
 				<div className="sidebar_headerRight">
 					{/* For the icons below we are going to 
 					wrap each icon inside IconButton. 
@@ -32,11 +34,18 @@ function Sidebar() {
 					</IconButton>
 				</div>
 			</div>
+			{/*  */}
 			<div className="sidebar_search">
 				<div className="sidebar_searchContainer">
 					<SearchOutlined />
 					<input placeholder="search or start new chat" type="text" />
 				</div>
+			</div>
+			{/*  */}
+			<div className="sidebar_chats">
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
 			</div>
 		</div>
 	);
---
+++ src/SidebarChat.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SidebarChat.css";
+
+function SidebarChat() {
+	return (
+		<div className="sidebarChat">
+			{/* <Avatar /> */}
+			<div className="sidebarChat_info">
+				<h2> Room name </h2>
+				<p> This is the last message </p>
+			</div>
+		</div>
+	);
+}
+
+export default SidebarChat;
