2ebb5ec25759a749a025f6f6fe1812ede000fffc
code <mmahad280@gmail.com>
2020-09-08 20:57:57 -0400

    styles for sidebar chat

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SidebarChat.css";
 
+import { Avatar } from "@material-ui/core";
+
 function SidebarChat() {
 	return (
 		<div className="sidebarChat">
-			{/* <Avatar /> */}
+			<Avatar />
 			<div className="sidebarChat_info">
 				<h2> Room name </h2>
 				<p> This is the last message </p>
